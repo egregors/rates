3a7b16a27c6baed5e26e92b38864da4ba0f8a33e
egregors <egregors@pm.me>
2024-02-10 14:11:34 +0100

    add bootstrap, demo page

--- internal/server/web/server.go
+++ internal/server/web/server.go
@@ -1,12 +1,15 @@
 package web
 
 import (
+	"embed"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"html/template"
 	"net/http"
 	"sort"
+	"strconv"
+	"strings"
 
 	srv "github.com/egregors/rates/internal/server"
 )
@@ -16,6 +19,9 @@ var tmpl = template.Must(template.ParseFiles(
 	"internal/server/web/templates/index.gohtml",
 	"internal/server/web/templates/rates-form-and-history.gohtml",
 ))
+
+//go:embed static
+var static embed.FS
 
 type RateReq struct {
 	From, To, Rate string
@@ -55,7 +61,7 @@ func New(rp srv.RateProvider, l srv.Logger) *Server {
 	s.r.Get("/", s.index)
 	s.r.Post("/rates", s.getRatesAndHistory)
 
-	s.r.Handle("/static/*", http.StripPrefix("/static", http.FileServer(http.Dir("internal/server/web/static"))))
+	s.r.Handle("/static/*", http.FileServer(http.FS(static)))
 
 	s.currencies = s.prepareCurrencies()
 
@@ -79,8 +85,13 @@ func (s *Server) index(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) getRatesAndHistory(w http.ResponseWriter, r *http.Request) {
-	from := r.FormValue("from")
-	to := r.FormValue("to")
+	prompt := r.FormValue("prompt")
+	from, to, amount, err := parsePrompt(prompt)
+	if err != nil {
+		s.l.Printf("failed to parse prompt: %v", err)
+		http.Error(w, "invalid prompt", http.StatusBadRequest)
+		return
+	}
 
 	rate, err := s.rp.GetRate(from, to)
 	if err != nil {
@@ -90,7 +101,14 @@ func (s *Server) getRatesAndHistory(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	s.histories[r.RemoteAddr] = append(s.histories[r.RemoteAddr], RateReq{from, to, fmt.Sprintf("%.2f", rate)})
+	s.histories[r.RemoteAddr] = append(
+		s.histories[r.RemoteAddr],
+		RateReq{
+			from,
+			to,
+			fmt.Sprintf("%.2f %s -> %.2f %s", amount, from, amount*rate, to),
+		},
+	)
 
 	if err := tmpl.Execute(w, Data{
 		Currencies: s.currencies,
@@ -99,6 +117,20 @@ func (s *Server) getRatesAndHistory(w http.ResponseWriter, r *http.Request) {
 		s.l.Printf("failed to render template: %v", err)
 		http.Error(w, "failed to render template", http.StatusInternalServerError)
 	}
+}
+
+func parsePrompt(prompt string) (from, to string, amount float64, err error) {
+	xs := strings.Split(prompt, " ")
+	if len(xs) != 4 {
+		return "", "", 0, fmt.Errorf("invalid prompt: %s", prompt)
+	}
+
+	amount, err = strconv.ParseFloat(xs[0], 64)
+	if err != nil {
+		return "", "", 0, fmt.Errorf("invalid amount: %s", xs[0])
+	}
+
+	return strings.ToLower(xs[1]), strings.ToLower(xs[3]), amount, nil
 }
 
 func (s *Server) prepareCurrencies() []Currency {
